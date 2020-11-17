0ff4c0511d73898548d201ed47b475f32b531feb
Yididiya M. Gudisa <yididiyamulatu@gmail.com>
2020-11-17 14:32:12 +0100

    editing login page, added background color

--- src/components/auth/LoginPage.jsx
+++ src/components/auth/LoginPage.jsx
@@ -3,6 +3,9 @@ import Auth from "../../services/Auth";
 import LoginForm from "./LoginForm";
 import RegisterForm from "./RegisterForm";
 
+//Styling 
+import loginPage from "..//../css/Components/loginPage.css";
+
 function LoginPage() {
     const login = async (loginData) => {
         const loginSuccess = await Auth.login(loginData);
@@ -20,27 +23,13 @@ function LoginPage() {
 
     return (
         <div className="wrapper">
-            <div className="container">
-                <div className="row mt-4">
-                    <div className="col-md-6 " style={{ color: "white" }}>
-                        <h1>SDA</h1>
-                        <p>Starter template</p>
-                    </div>
-
-                    <div className="col-md-6">
-                        <div className="row">
-                            <div className="col-12  strong-shadow">
+                        <div className="loginForm">
                                 <LoginForm onSubmit={login} />
-                            </div>
-
-                            <div className="col-12 mt-4">
+                           {/* <div className="col-12 mt-4">
                                 <RegisterForm onSubmit={register} />
-                            </div>
+    </div>*/}
                         </div>
                     </div>
-                </div>
-            </div>
-        </div>
     );
 }
 
